d1285b8edd6125deb2da2fe14e64cd3f4d9e0e8c
Martavias Cary <martavias.cary@outlook.com>
2026-08-11 14:11:30 -0400

    Initial commit

---
+++ src/main/java/com/marsprogramminglab/stockpot/StockpotApplication.java
@@ -0,0 +1,13 @@
+package com.marsprogramminglab.stockpot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockpotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockpotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marsprogramminglab/stockpot/StockpotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marsprogramminglab.stockpot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockpotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
